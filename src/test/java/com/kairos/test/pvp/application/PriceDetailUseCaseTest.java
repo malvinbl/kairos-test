e96374a0aed72d28e21d28c10023a7df43517ac5
Malvin <malvinbl@gmail.com>
2025-05-20 20:09:41 +0200

    test: add unit tests to price detail use case

---
+++ src/test/java/com/kairos/test/pvp/application/PriceDetailUseCaseTest.java
@@ -0,0 +1,52 @@
+package com.kairos.test.pvp.application;
+
+import com.kairos.test.pvp.domain.model.PriceDetail;
+import com.kairos.test.pvp.domain.repository.PriceRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.time.LocalDateTime;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+class PriceDetailUseCaseTest {
+
+    @Mock
+    private PriceRepository priceRepository;
+
+    @InjectMocks
+    private PriceDetailUseCase priceService;
+
+    @Test
+    void should_get_priceDetail_given_productId_and_brandId_and_date() {
+        Long productId = 35455L;
+        Long brandId = 1L;
+
+        LocalDateTime date = LocalDateTime.of(2020,6,14, 10, 0);
+
+        LocalDateTime startDate = LocalDateTime.of(2020,6,14, 0, 0);
+        LocalDateTime endDate = LocalDateTime.of(2020,12,31, 23, 59);
+
+        PriceDetail expectedResponse = new PriceDetail(
+                startDate,
+                endDate,
+                productId,
+                brandId,
+                1L,
+                0,
+                35.50
+        );
+
+        when(priceRepository.findPricingDetails(productId, brandId, date)).thenReturn(Stream.of(expectedResponse));
+
+
+        PriceDetail actualResponse = priceService.getPriceDetail(productId, brandId, date);
+
+
+        assertThat(actualResponse).usingRecursiveComparison().isEqualTo(expectedResponse);
+    }
+
+}
