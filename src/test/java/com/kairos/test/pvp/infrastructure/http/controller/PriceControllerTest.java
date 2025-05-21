87df2985f3014f602bb013954c939d7ef9a2bc14
Malvin <malvinbl@gmail.com>
2025-05-21 10:53:01 +0200

    test: add price controller integration test

--- src/test/java/com/kairos/test/pvp/infrastructure/http/controller/PriceControllerTest.java
+++ src/test/java/com/kairos/test/pvp/infrastructure/http/controller/PriceControllerTest.java
@@ -45,4 +45,89 @@ class PriceControllerTest extends AbstractIntegrationTest {
                 .andReturn().getResponse().getContentAsString();
     }
 
+    @Test
+    void test_2() throws Exception {
+        mockMvc.perform(
+                        get(URL).contentType("application/json")
+                                .param("date", "2020-06-14T16:00:00")
+                                .param("productId", "35455")
+                                .param("brandId", "1")
+                )
+                .andExpect(status().is(HttpStatus.OK.value()))
+                .andExpect(jsonPath("$.productId", is(35455)))
+                .andExpect(jsonPath("$.brandId", is(1)))
+                .andExpect(jsonPath("$.priceList", is(2)))
+                .andExpect(jsonPath("$.startDate", is("2020-06-14T15:00:00")))
+                .andExpect(jsonPath("$.endDate", is("2020-06-14T18:30:00")))
+                .andExpect(jsonPath("$.price", is(25.45)))
+                .andReturn().getResponse().getContentAsString();
+    }
+
+    @Test
+    void test_3() throws Exception {
+        mockMvc.perform(
+                        get(URL).contentType("application/json")
+                                .param("date", "2020-06-14T21:00:00")
+                                .param("productId", "35455")
+                                .param("brandId", "1")
+                )
+                .andExpect(status().is(HttpStatus.OK.value()))
+                .andExpect(jsonPath("$.productId", is(35455)))
+                .andExpect(jsonPath("$.brandId", is(1)))
+                .andExpect(jsonPath("$.priceList", is(1)))
+                .andExpect(jsonPath("$.startDate", is("2020-06-14T00:00:00")))
+                .andExpect(jsonPath("$.endDate", is("2020-12-31T23:59:59")))
+                .andExpect(jsonPath("$.price", is(35.50)))
+                .andReturn().getResponse().getContentAsString();
+    }
+
+    @Test
+    void test_4() throws Exception {
+        mockMvc.perform(
+                        get(URL).contentType("application/json")
+                                .param("date", "2020-06-15T10:00:00")
+                                .param("productId", "35455")
+                                .param("brandId", "1")
+                )
+                .andExpect(status().is(HttpStatus.OK.value()))
+                .andExpect(jsonPath("$.productId", is(35455)))
+                .andExpect(jsonPath("$.brandId", is(1)))
+                .andExpect(jsonPath("$.priceList", is(3)))
+                .andExpect(jsonPath("$.startDate", is("2020-06-15T00:00:00")))
+                .andExpect(jsonPath("$.endDate", is("2020-06-15T11:00:00")))
+                .andExpect(jsonPath("$.price", is(30.50)))
+                .andReturn().getResponse().getContentAsString();
+    }
+
+    @Test
+    void test_5() throws Exception {
+        mockMvc.perform(
+                        get(URL).contentType("application/json")
+                                .param("date", "2020-06-16T21:00:00")
+                                .param("productId", "35455")
+                                .param("brandId", "1")
+                )
+                .andExpect(status().is(HttpStatus.OK.value()))
+                .andExpect(jsonPath("$.productId", is(35455)))
+                .andExpect(jsonPath("$.brandId", is(1)))
+                .andExpect(jsonPath("$.priceList", is(4)))
+                .andExpect(jsonPath("$.startDate", is("2020-06-15T16:00:00")))
+                .andExpect(jsonPath("$.endDate", is("2020-12-31T23:59:59")))
+                .andExpect(jsonPath("$.price", is(38.95)))
+                .andReturn().getResponse().getContentAsString();
+    }
+
+    @Test
+    void test_malformed_json() throws Exception {
+        mockMvc.perform(
+                        get(URL).contentType("application/json")
+                                .param("date", "2020-06-16z21:00:00")
+                                .param("productId", "35455")
+                                .param("brandId", "1")
+                )
+                .andExpect(status().is(HttpStatus.BAD_REQUEST.value()))
+                .andExpect(jsonPath("$.error", is("Malformed JSON request")))
+                .andReturn().getResponse().getContentAsString();
+    }
+
 }
